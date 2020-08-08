978857880011c77356f723e7a7d5fd9a9aeadb86
Jorge Garcia <jorgarcia1994@gmail.com>
2020-08-08 13:32:55 -0700

    created buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Description from './Description.jsx';
 import Rating from './Rating.jsx';
 import Location from './Location.jsx';
+import Buttons from './Buttons.jsx';
 
 const Header = () => {
   const [data, setData] = useState({})
@@ -12,6 +13,7 @@ const Header = () => {
       <div>
         <Rating starRating="4.95" totalReviews="69" />
         <Location location="RSM, CA United States" />
+        <Buttons />
       </div>
 
     </div>
