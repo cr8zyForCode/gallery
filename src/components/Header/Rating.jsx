324d184b82a3d0269ab892a4d737195fc538b8f4
Jorge Garcia <jorgarcia1994@gmail.com>
2020-08-10 21:05:07 -0700

    refactored rating

--- src/components/Header/Rating.jsx
+++ src/components/Header/Rating.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-//import data with a call
+
+import styles from './Rating.scss';
 
 const Rating = ({ starRating, totalReviews }) => {
-  let containerStyle = {
-    display: 'flex',
-  };
-  let starStyle = {
-    color: '#ff385c'
-  };
-  let totalReviewStyles = {
-    cursor: 'pointer',
-    color: '#717171',
-    paddingLeft: '4px'
-  };
-  let starRatingStyles = {
-    cursor: 'pointer',
-    paddingLeft: '4px'
-  }
   return (
-    <div className="rating" style={containerStyle}>
-      <ion-icon name="star" style={starStyle} />
-      <p style={starRatingStyles}>{starRating}</p>
-      <p style={totalReviewStyles}>({totalReviews})</p>
+    <div className={styles.container} >
+      <ion-icon name="star" />
+      <p className={styles.rating}>{starRating}</p>
+      <p className={styles.reviews}>({totalReviews})</p>
     </div>
   )
 }
