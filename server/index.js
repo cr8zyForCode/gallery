b36597e44dbb7d05bcdcdc58ee9b0fbfc7ffbb3b
Jorge Garcia <jorgarcia1994@gmail.com>
2020-08-11 18:27:15 -0700

    modified the model

--- server/index.js
+++ server/index.js
@@ -5,17 +5,22 @@ const PORT = 3000;
 
 //import database
 const db = require('../db/index');
+//import query models
+let propertyQueries = require('./models/properties');
 
 app.use(express.json());
 //send static files inside the public folder
-// app.use(express.static(path.join(__dirname, 'public')));
-let propertyQueries = require('./models/properties');
-app.get('/properties', (req, res) => {
-  propertyQueries.getProperties((err, documents) => {
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/properties/:id', (req, res) => {
+  // console.log(req.params._id);
+  let _id = req.params._id
+  propertyQueries.getProperties(_id, (err, house) => {
     if (err) {
       res.status(400).send('error');
     } else {
-      res.status(200).send(documents);
+      res.status(200).send(house);
     }
   });
 });
