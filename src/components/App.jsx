57c24bff605a122a4790a07f0e965517adf0aa07
Jorge Garcia <jorgarcia1994@gmail.com>
2020-08-12 22:43:11 -0700

    refactored app to use a ternary statement to display html page after request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,29 +8,20 @@ import Modal from './Modal.jsx';
 import styles from './App.scss';
 
 export default () => {
-  const [description, setDescription] = useState('');
-  const [starRating, setStarRating] = useState('');
-  const [reviewTotal, setReviewTotal] = useState('');
   const [superhost, setSuperhost] = useState('');
-  const [location, setLocation] = useState('');
   const [images, setImages] = useState({});
-  const [image4, setImage4] = useState('');
-  const [image5, setImage5] = useState('');
-  // const [home, setHome] = useState({});
+  const [home, setHome] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  const [isShowingAllImages, setIsShowingAllImages] = useState(false);
 
   let getHouse = () => {
     let id = Math.floor(Math.random() * 20);
     axios.get(`/properties/${id}`)
       .then((house) => {
-        setDescription(house.data[0].description);
-        setStarRating(house.data[0].starRating);
-        setReviewTotal(house.data[0].reviewTotal);
         setSuperhost(house.data[0].superhost);
-        setLocation(house.data[0].location);
         setImages(house.data[0].images);
-        setImage4(house.data[0].images.bedrooms[0]);
-        setImage5(house.data[0].images.bathrooms[0]);
-        // setHome(house.data[0]);
+        setHome(house.data[0]);
+        setLoading(false);
       })
       .catch(console.log)
   }
@@ -39,22 +30,25 @@ export default () => {
     getHouse()
   }, []);
 
+  if (isLoading) {
+    return (<div>...</div>)
+  }
   return (
     <div className={styles.app}>
-      <Header description={description} starRating={starRating} totalReviews={reviewTotal} location={location} />
+      <Header description={home.description} starRating={home.starRating} totalReviews={home.reviewTotal} location={home.location} />
       <div className={styles.images}>
         <Image image={images.house} w="560" h="310" size='large' />
         <div className={styles.small}>
           <Image image={images.backyard} w="270" h="150" />
           <Image image={images.kitchen} w="270" h="150" />
-          <Image image={image4.imageURL} w="270" h="150" />
+          <Image image={images.bedrooms[0].imageURL} w="270" h="150" />
           <div className={styles['image-button']}>
-            <Image image={image5.imageURL} w="270" h="150" />
+            <Image image={images.bathrooms[0].imageURL} w="270" h="150" />
             <button className={styles.button}>Show all photos</button>
           </div>
         </div>
       </div>
-      <Modal imgNumber={4} totalImages={20} />
+      {isShowingAllImages ? (<Modal imgNumber={4} totalImages={20} />) : null}
     </div >
   )
 }
