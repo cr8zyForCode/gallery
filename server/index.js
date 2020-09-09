792afdbc49f4e3df49b8da899adbff8c0b5f40d4
anton10Luna <antonio.gilluna@gmail.com>
2020-09-09 12:39:43 -0700

    update to server index and package json

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3001;
-require('newrelic');
+// require('newrelic');
 
 //import database
 const db = require('../db/index');
